5fe814a0bcab0483154b6530c64379818e4487f7
Jakub Pitner <jakub.pitner128@seznam.cz>
2023-11-18 12:45:29 +0100

    Theme switcher

--- src/public/themeSwitcher.js
+++ src/public/themeSwitcher.js
@@ -1,13 +1,49 @@
-function toogleTheme() {
-    var theme = document.getElementsByTagName("link")[4];
-    var logo = document.getElementById("logo");
-    var icons = document.getElementById("icons");
-    if(theme.getAttribute('href') == 'light.css') {
-        theme.setAttribute('href', '../public/styles/dark.css')
-        logo.setAttribute('src', "../public/images/logo_dark.png");
-        icons.setAttribute('src', '#')
-        console.log("THEME DARK")
-    } else {
-        theme.setAttribute('href' ,'../public/styles/light.css');
+class ThemeManager {
+    static getDOM() {
+        var logo = document.getElementById("logo");
+        var icons = document.getElementById("icons");
+        var theme = document.getElementsByTagName("link")[4];
+        return [logo, icons, theme];
+    }
+
+    static getSystemTheme() {
+        var systemTheme;
+        if(window.matchMedia('(prefers-color-scheme: dark)').matches === true) {
+            systemTheme = "dark";
+        } else {
+            systemTheme = "light";
+        }
+        return systemTheme;
+    }
+
+    static setThemeBySystem() {
+        var documentArray = this.getDOM();
+        var systemTheme = this.getSystemTheme();
+        console.log(documentArray);
+        if(systemTheme === "dark") {
+            documentArray[2].setAttribute("href", "../public/styles/dark.css");
+            documentArray[0].setAttribute("src", "../public/images/logo_dark.png");
+            documentArray[1].setAttribute("src", "../public/images/icons_dark.png");
+        } else {
+            documentArray[2].setAttribute("href", "../public/styles/light.css");
+            documentArray[0].setAttribute("src", "../public/images/logo_light.png");
+            documentArray[1].setAttribute("src", "../public/images/icons_light.png");
+        }
+
+        return "Theme Changed";
+    }
+
+    static setTheme() {
+        var documentArray = this.getDOM();
+        if(documentArray[2].getAttribute("href") == "../public/styles/light.css") {
+            documentArray[2].setAttribute("href", "../public/styles/dark.css");
+            documentArray[0].setAttribute("src", "../public/images/logo_dark.png");
+            documentArray[1].setAttribute("src", "../public/images/icons_dark.png");
+        } else {
+            documentArray[2].setAttribute("href", "../public/styles/light.css");
+            documentArray[0].setAttribute("src", "../public/images/logo_light.png");
+            documentArray[1].setAttribute("src", "../public/images/icons_light.png");
+
+        }
     }
 }
