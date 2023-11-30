e5682347ce7e83632a9c218de9a082db38f39493
goodbyepavlyi <goodbyepavlyi@gmail.com>
2023-11-30 14:57:37 +0100

    Refactor error handling and shutdown process

--- src/app.js
+++ src/app.js
@@ -1,91 +1,68 @@
 const Logger = require("./lib/Logger");
 const Core = require("./lib/Core");
 
-/**
- * Create a new instance of the Core class.
- * @type {Core}
- */
 const core = new Core();
 
 /**
  * Handle unhandled Promise rejections by logging the error.
- * @event
  * @param {any} reason - The reason for the unhandled rejection.
  * @param {Promise} promise - The unhandled Promise.
  */
 process.on("unhandledRejection", (reason, promise) => {
-    core.getLogger().error(Logger.Type.Watchdog, "unhandledRejection", {
-        reason: reason,
+    core.getLogger().error(Logger.Type.Watchdog, "Unhandled rejection occured:", {
+        reason,
         stack: reason?.stack,
-        promise: promise
+        promise,
     });
 });
 
 /**
  * An asynchronous function to gracefully terminate the process.
- * @function
  * @async
  */
 async function shutdown() {
     try {
-        /**
-         * Shutdown the application gracefully.
-         * @method
-         * @async
-         */
-        await core.shutdown();
-
-        // Exit the process with a status code of 0 (success) after a successful shutdown.
         process.exit(0);
-    } catch (err) {
-        // Handle errors that occur during shutdown.
-        core.getLogger().error(Logger.Type.Watchdog, "Error occurred: ", err.name, " - ", err.message);
-        core.getLogger().error(Logger.Type.Watchdog, err.stack);
+    } catch (error) {
+        core.getLogger().error(Logger.Type.Watchdog, "An error occured during shutdown: ", {
+            name: error.name,
+            stack: error.stack,
+        });
 
-        // Exit the process with a status code of 1 (error) if an error occurred during shutdown.
         process.exit(1);
     }
 }
 
-/**
- * Signal termination handler - used if the process is killed (e.g., SIGTERM signal).
- * @event
- */
+// Signal termination handler - used if the process is killed (e.g., SIGTERM signal).
 process.on("SIGTERM", shutdown);
 
-/**
- * Signal interrupt handler - used if the process is aborted by Ctrl + C (e.g., SIGINT signal, during development).
- * @event
- */
+// Signal interrupt handler - used if the process is aborted by Ctrl + C (e.g., SIGINT signal, during development).
 process.on("SIGINT", shutdown);
 
 /**
  * Handle uncaught exceptions by logging the error and triggering the shutdown process.
- * @event
- * @param {Error} err - The uncaught exception error.
+ * @param {Error} error - The uncaught exception error.
  * @param {string} origin - The origin of the exception.
  */
-process.on("uncaughtException", (err, origin) => {
-    core.getLogger().error(Logger.Type.Watchdog, "Uncaught Exception", {
-        err: err,
-        origin: origin
+process.on("uncaughtException", (error, origin) => {
+    core.getLogger().error(Logger.Type.Watchdog, "Uncaught exception occured:", {
+        error,
+        origin,
     });
 
     // Trigger the shutdown process and ignore any errors that may occur during shutdown.
-    shutdown().catch(() => {
-        /* intentional */
-    });
+    shutdown().catch(() => {});
 });
 
 /**
  * Handle the exit event of the process to log the stack trace if it exits with a non-zero status code.
- * @event
  * @param {number} code - The exit status code.
  */
-process.on("exit", function(code) {
+process.on("exit", (code) => {
     if (code !== 0) {
         core.getLogger().error(Logger.Type.Watchdog, "Stack trace that led to the process exiting with code " + code + ":", new Error().stack);
-    } else {
-        core.getLogger().info(Logger.Type.Watchdog, "Exiting with code " + code + "...");
+        return;
     }
+   
+    core.getLogger().info(Logger.Type.Watchdog, "Exiting with code " + code + "...");
 });
