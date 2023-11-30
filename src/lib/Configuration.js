5e4ded67ab82329cd17a80f7295c23b3c2547b98
goodbyepavlyi <goodbyepavlyi@gmail.com>
2023-11-30 15:02:58 +0100

    Refactor Configuration methods jsdoc comments

--- src/lib/Configuration.js
+++ src/lib/Configuration.js
@@ -1,11 +1,14 @@
 class Configuration {
     /**
      * Get the port number for the Webserver
-     * @static
      * @returns {number} The Webserver port number.
      */
     getWebserverPort = () => process.env.WEBSERVER_PORT || 3000;
 
+    /**
+     * Get the log level for the application.
+     * @returns {string} The log level.
+     */
     getLogLevel = () => process.env.LOG_LEVEL || "info";
 }
 
