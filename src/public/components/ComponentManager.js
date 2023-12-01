785532c672aa515a1c60d79094847a22ef14f895
Jakub Pitner <jakub.pitner128@seznam.cz>
2023-12-01 17:24:59 +0100

    Changed styles and prepared Components

---
+++ src/public/components/ComponentManager.js
@@ -0,0 +1,22 @@
+class ComponentManager {
+    static getContent(uuid) {
+        axios.get("http://localhost:3000/api/lecturers/" + uuid)
+        .then(function(response) {
+            return response.data;
+        })
+        .catch(function(error) {
+            console.error(error);
+        })
+    }
+    
+    static createAndAppend(tag, content, appendTo) {
+        let newTag = document.createElement(tag);
+        if(tag != "img") {
+            newTag.innerHTML = content;
+        } else {
+            newTag.src = content;
+        }
+        parent = document.getElementById(appendTo);
+        parent.appendChild(newTag);
+    }
+}
--- src/public/components/Lecturer.js
+++
@@ -1,5 +0,0 @@
-// Lecturer is profile of each lecturer
-// Props: Title before, Full Name, Title After
-const Lecturer = (props) => {
-    return React.createElement("h2", null, props.firstTitle + " " + props.fullName + " " + props.secondTitle);
-}
