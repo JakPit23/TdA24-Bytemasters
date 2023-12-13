811ac33aa169ba8c5f7331e8e2b20754a0972964
goodbyepavlyi <goodbyepavlyi@gmail.com>
2023-12-13 18:23:44 +0100

    Refactor lecturer route to handle non-existent lecturer UUIDs

--- src/lib/webserver/routers/WebRoute.js
+++ src/lib/webserver/routers/WebRoute.js
@@ -19,6 +19,14 @@ module.exports = class WebRoute {
     loadRoutes = () => {
         this.router.get("/", (req, res) => res.sendFile(path.join(__dirname, "../../../views/index.html")));
 
-        this.router.get("/lecturer/:lecturerUUID", (req, res) => res.sendFile(path.join(__dirname, "../../../views/lecturer_template.html")));
+        this.router.get("/lecturer/:lecturerUUID", (req, res) => {
+            const { lecturerUUID } = req.params;
+
+            if (!this.core.getLecturerManager().getLecturer(lecturerUUID)) {
+                return res.status(404).sendFile(path.join(__dirname, "../../../views/404_lecturer.html"))
+            }
+
+            res.sendFile(path.join(__dirname, "../../../views/lecturer_template.html"))
+        });
     }
 };
