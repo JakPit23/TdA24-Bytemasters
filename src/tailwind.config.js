b84a1eac48ad0d81080406c857fd9c395b8fd61e
goodbyepavlyi <goodbyepavlyi@gmail.com>
2024-01-04 18:08:07 +0100

    Add build:css script to package.json and update tailwind.config.js

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html, js}"],
+  content: [
+    "./views/**/*.html",
+    "./public/**/*.js"
+  ],
   theme: {
     extend: {},
   },
