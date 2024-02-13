c48e350ed7bfe637ba462a3035a1564924b90ee3
Jakub Pitner <jakub.pitner128@seznam.cz>
2024-02-13 13:08:52 +0100

    Registrační formulář v1

--- src/lib/webserver/routers/WebRoute.js
+++ src/lib/webserver/routers/WebRoute.js
@@ -20,7 +20,7 @@ module.exports = class WebRoute {
         this.router.get("/", (req, res) => res.render("index"));
         this.router.get("/gdpr", (req, res) => res.render("gdpr"));
         this.router.get("/contact", (req, res) => res.render("contact"));
-        this.router.get("/register", (req, res) => res.send("register"));
+        this.router.get("/register", (req, res) => res.render("register"));
         this.router.get("/login", (req, res) => res.send("login"));
         this.router.get("/lecturer/:lecturerUUID", (req, res) => res.render("lecturer"));
     }
