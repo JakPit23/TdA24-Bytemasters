c66110d53d913e3588a2f6c96fac2b8ff87ec98c
goodbyepavlyi <goodbyepavlyi@gmail.com>
2024-02-19 14:41:01 +0100

    log response (ale nvm jak to brm brmuje)

--- src/lib/webserver/middlewares/RequestLog.js
+++ src/lib/webserver/middlewares/RequestLog.js
@@ -12,8 +12,32 @@ module.exports = class RequestLog {
             Logger.info(Logger.Type.Webserver, `${req.headers['x-forwarded-for'] || req.ip} - "${req.method} ${req.url}" ${req.headers['user-agent']}`);
         }
 
-        if (Config.getLogLevel() == "debug" && req.body) {
-            Logger.debug(Logger.Type.Webserver, `Request body:`, req.body);
+        if (Config.getLogLevel() == "debug") {
+            if (req.body) {
+                Logger.debug(Logger.Type.Webserver, `Request body:`, req.body);
+            }
+
+            // absolutne ale jako fakt absolutne nechapu jak to dela brm brm (z casti)
+            const oldWrite = res.write, oldEnd = res.end;
+            let chunks = [];
+            
+            res.write = function (chunk) {
+                chunks.push(chunk);
+                return oldWrite.apply(res, arguments);
+            };
+
+            res.end = function (chunk) {
+                if (chunk) {
+                    chunks.push(chunk);
+                }
+
+                const body = Buffer.concat(chunks).toString('utf8');
+                if (body) {
+                    Logger.debug(Logger.Type.Webserver, `Response body:`, body);
+                }
+
+                oldEnd.apply(res, arguments);
+            };
         }
 
         return next();
