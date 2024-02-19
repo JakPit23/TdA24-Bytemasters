d365d13a69c84f94a6368c231d56c78d980ec9dc
Jakub Pitner <jakub.pitner128@seznam.cz>
2024-02-19 16:42:57 +0100

    Kaledář v2 - stále nefunguje

--- src/public/js/pages/Dashboard.js
+++ src/public/js/pages/Dashboard.js
@@ -1,6 +1,8 @@
 class Dashboard {
     constructor(app) {
         this.app = app;
+        this.caledarEl = document.getElementById('calendar');
+        this.freeTime = $(['data-freeTime']);
         this.init();
     }
 
@@ -9,25 +11,60 @@ class Dashboard {
     }
 
     calendar = async () => {
-        document.addEventListener('DOMContentLoaded', function() {
-            var calendarEl = document.getElementById('calendar');
-            var calendar = new FullCalendar.Calendar(calendarEl, {
-              initialView: 'dayGridMonth',
-              locale: 'eu',
-              firstDay: 1,
-              buttonText: {
-                today: 'Dnes',
-                month: 'Měsíc',
-                week: 'Týden',
-                day: 'Den',
-                list: 'Seznam'
-              },
-              fixedWeekCount: false,
-            });
-            calendar.render();
-          });
-    
-    }
-}
+      const Calendar = tui.Calendar;
+      
+      // ToastUI calendar - month view
+      const calendar = new Calendar(this.caledarEl, {
+        defaultView: 'month',
+        taskView: false,
+        scheduleView: true,
+        useCreationPopup: true,
+        useDetailPopup: true,
+        template: {
+          monthDayname: function(dayname) {
+            return '<span class="calendar-week-dayname-name">' + dayname.label + '</span>';
+          },
+          monthDayname: function(model) {
+            return '<span class="tui-full-calendar-weekday-grid-date tui-full-calendar-weekday-grid-date-' + model.day + '">' + model.renderDate + '</span>';
+          }
+        },
+      });
 
+      // next and prev month
+      const prevBtn = document.getElementById('prev');
+      const nextBtn = document.getElementById('next');
+      prevBtn.addEventListener('click', function() {
+        calendar.prev();
+      });
+
+      nextBtn.addEventListener('click', function() {
+        calendar.next();
+      });
+
+      // Switch to week selected in month view
+      calendar.on('clickSchedule', function(event) {
+        const schedule = event.schedule;
+        console.log(schedule);
+        calendar.changeView('week', true);
+      });
+
+      calendar.setTheme({
+        common: {
+          backgroundColor: '#333',
+          border: '1px solid #fff',
+          gridSelection: {
+            backgroundColor: '#333',
+            border: '1px solid rgba(254, 203, 46, 1)'
+          },
+          dayName: {color: '#fff'},
+          holiday: {color: '#fff'},
+          saturday: {color: '#fff'},
+          today: {color: '#fff', backgroundColor: 'rgba(254, 203, 46, 1)'}
+        },
+        week: {
+          nowIndicatorBullet: {backgroundColor: 'rgba(254, 203, 46, 1)'}
+        }
+      }) 
+
+  }}
 this.cal = new Dashboard();
