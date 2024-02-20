33aa002f5eada40d046b29ca6713911f1bca85ee
Jakub Pitner <jakub.pitner128@seznam.cz>
2024-02-20 17:04:45 +0100

    Duplicate test fix

--- src/public/js/pages/DashboardPage.js
+++ src/public/js/pages/DashboardPage.js
@@ -2,14 +2,6 @@ class DashboardPage {
     constructor(app) {
         this.app = app;
         this.calendar = new CalendarModule();
-        this.calendar.test();
-    }
-    
-    test() {
-      this.createCalendar(this.calendarElement[0]);
-      this.renderCalendar();
-      this.createAllDayEvent('test', '2024-02-21');
-      this.createDraggable(this.draggableElement[0]);
     }
 }
 
