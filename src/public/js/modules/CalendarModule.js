6e17fd0aeb0127017fb25174389d8e6b114fce37
goodbyepavlyi <goodbyepavlyi@gmail.com>
2024-02-20 16:51:12 +0100

    kalendar

---
+++ src/public/js/modules/CalendarModule.js
@@ -0,0 +1,59 @@
+class CalendarModule {
+    constructor() {
+        this.Calendar = FullCalendar.Calendar;
+        this.Draggable = FullCalendar.Draggable;
+        this.test();
+    }
+
+    createCalendar(calendarElement) {
+        let calendar = new this.Calendar(calendarElement, {
+            locale: 'cs',
+            firstDay: 1,
+            editable: true,
+            droppable: true,
+            dayMaxEvents: true,
+        });
+
+        this.calendar = calendar;
+    }
+
+    renderCalendar() {
+        this.calendar.setOption('editable', false);
+        this.calendar.render();
+    }
+
+    createAllDayEvent(title, date) {
+        this.calendar.addEvent({
+            title: title,
+            start: date,
+            allDay: true
+        });
+    }
+
+    createDraggable(element) {
+        new this.Draggable(element, {
+            eventData: function (eventEl) {
+                return {
+                    title: eventEl.innerText,
+                    date: '2024-02-21'
+                };
+            }
+        });
+
+    }
+
+    test() {
+        this.createCalendar(document.getElementById('calendar'));
+        this.renderCalendar();
+        this.createAllDayEvent('test', '2024-02-21');
+        this.createDraggable(document.getElementById('external-events'));
+    }
+
+    getEvents = () => {
+        return this.calendar.getEvents().map(event => ({
+            title: event.title,
+            start: event.start.getTime(),
+            end: event.end.getTime(),
+        }));
+    }
+}
--- src/public/js/pages/DashboardPage.js
+++ src/public/js/pages/DashboardPage.js
@@ -1,46 +1,8 @@
 class DashboardPage {
     constructor(app) {
         this.app = app;
-        this.Calendar = FullCalendar.Calendar;
-        this.Draggable = FullCalendar.Draggable;
-        this.test();
-    }
-    
-    createCalendar(calendarElement) {
-      let calendar = new this.Calendar(calendarElement, {
-        locale: 'cs',
-        firstDay: 1,
-        editable: true,
-        droppable: true,
-        dayMaxEvents: true,
-      });
-
-      this.calendar = calendar;
-    }
-
-    renderCalendar() {
-      this.calendar.setOption('editable', false);
-      this.calendar.render();
-    }
-
-    createAllDayEvent(title, date) {
-      this.calendar.addEvent({
-        title: title,
-        start: date,
-        allDay: true
-      });
-    }
-
-    createDraggable(element) {
-      new this.Draggable(element, {
-        eventData: function(eventEl) {
-          return {
-            title: eventEl.innerText,
-            date: '2024-02-21'
-          };
-        }
-      });
-    
+        this.calendar = new CalendarModule();
+        this.calendar.test();
     }
     
     test() {
