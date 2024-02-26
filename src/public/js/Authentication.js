e309d3e3c1f3409c54c008c2bebc96570ee23922
Jakub Pitner <jakub.pitner128@seznam.cz>
2024-02-26 16:59:12 +0100

    Errors working

--- src/public/js/Authentication.js
+++ src/public/js/Authentication.js
@@ -26,11 +26,11 @@ class Authentication {
         console.log(body)
         if(response.status !== 200) {
             var text = ""
-            if(body.error == "Invalid credentials") {
+            if(body.error == "INVALID_CREDENTIALS") {
                 console.log("ERROR")
                 text = $("<p></p>").text("Špatné uživatelské jméno nebo heslo").addClass('mx-auto p-4');
             } else if(body.error == "MISSING_REQUIRED_VALUES") {
-                text = $("<p></p>").text("Nezadali jste veškeré údaje");
+                text = $("<p></p>").text("Nezadali jste veškeré údaje").addClass('mx-auto p-4');
             }
             this.messagebox.append(text);
             this.messagebox.addClass('mt-3');
