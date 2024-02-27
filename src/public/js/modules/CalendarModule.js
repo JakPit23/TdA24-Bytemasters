9b661bbe9c8e09fc7f9bfe9ad12a8f8188498724
Jakub Pitner <jakub.pitner128@seznam.cz>
2024-02-27 13:40:04 +0100

    footer & contact

--- src/public/js/modules/CalendarModule.js
+++ src/public/js/modules/CalendarModule.js
@@ -20,7 +20,6 @@ class CalendarModule {
 
     createCalendar = async(calendarElement) => {
         let calendar = new this.Calendar(calendarElement, {
-            width: '100%',
             locale: 'cs',
             firstDay: 1,
             editable: true,
