17c75e30a31fbce7fef1cf3d02379393e15bab64
goodbyepavlyi <goodbyepavlyi@gmail.com>
2024-03-20 18:56:57 +0100

    proc tady dopcii todle chicpi

--- src/lib/types/APIError.js
+++ src/lib/types/APIError.js
@@ -19,6 +19,7 @@ module.exports = class APIError extends Error {
         InvalidValueLength: "InvalidValueLength",
         DuplicateValue: "DuplicateValue"
     }
+    
     static Unknown = new APIError({ message: "An unknown error occurred", type: APIError.Types.Unknown });
     static InvalidCredentials = new APIError({ message: "InvalidCredentials", type: APIError.Types.InvalidCredentials });
 
--- src/lib/webserver/APIResponse.js
+++ src/lib/webserver/APIResponse.js
@@ -22,17 +22,13 @@ module.exports = class APIResponse {
     static InternalServerError = new APIResponse({ statusCode: 500, code: 500, error: { type: "InternalServerError" } });
     static InvalidRequestBody = new APIResponse({ statusCode: 400, code: 400, error: { type: "InvalidRequestBody" } });
     static Unauthorized = new APIResponse({ statusCode: 401, code: 401, error: { type: "Unauthorized" } });
-
     static InvalidCredentials = new APIResponse({ statusCode: 400, code: 400, error: { type: "InvalidCredentials" } });
-
-    static TimeSlotNotAvailable = new APIResponse({ statusCode: 400, code: 400, error: { type: "TimeSlotNotAvailable" } });
-    static TimeConflict = new APIResponse({ statusCode: 400, code: 400, error: { type: "TimeConflict" } });
-    static InvalidDates = new APIResponse({ statusCode: 400, code: 400, error: { type: "InvalidDates" } });
+    
+    static KeyAlreadyExists = (key) => new APIResponse({ statusCode: 400, code: 400, error: { type: "KeyAlreadyExists", key } });
+    static KeyNotFound = (key) => new APIResponse({ statusCode: 404, code: 404, error: { type: "KeyNotFound", key } });
+    static KeyNotDeleted = (key) => new APIResponse({ statusCode: 500, code: 500, error: { type: "KeyNotDeleted", key } });
 
     static InvalidValueType = (value) => new APIResponse({ statusCode: 400, code: 400, error: { type: "InvalidValueType", value } });
     static InvalidValueLength = (value) => new APIResponse({ statusCode: 400, code: 400, error: { type: "InvalidValueLength", value } });
     static DuplicateValue = (value) => new APIResponse({ statusCode: 400, code: 400, error: { type: "DuplicateValue", value } });
-
-    static UserNotFound = new APIResponse({ statusCode: 400, code: 400, error: { type: "UserNotFound" } });
-    static UserAlreadyExists = new APIResponse({ statusCode: 400, code: 400, error: { type: "UserAlreadyExists" } });
 }
