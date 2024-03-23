e5921267cb7446e0894ffac48f669a14c643dfcb
goodbyepavlyi <goodbyepavlyi@gmail.com>
2024-03-23 15:16:17 +0100

    renamed smth

--- src/public/js/pages/IndexPage.js
+++ src/public/js/pages/IndexPage.js
@@ -1,102 +1,66 @@
 class Page {
-    /**
-     * @param {Application} app 
-     */
     constructor(app) {
         this.app = app;
         this.api = new API();
 
+        this.activitiesList = $("[data-activities]");
+        this.searchBar = $("[data-searchInput]");
+
         this.init();
     }
 
-    /**
-     * @private
-     */
-    async _fetchMoreLecturers() {
-        console.log('Loading more items...');
-        this._fetchInProgress = true;
-
-        const lecturers = await this._fetchLecturers({ limit: 25, after: this.lecturers[this.lecturers.length - 1].uuid });
-        if (!lecturers || lecturers.length <= 0) {
-            this.app.hideLoader('[data-loaderLecturers]');
-            console.log('No more items to load...');
-            return;
-        }
-
-        this.lecturers.push(...lecturers);
-        this.loadLecturers(lecturers);
-        this._fetchInProgress = false;
-    }
-
-    /**
-     * @private
-     */
-    async _fetchLecturers(options = {}) {
-        try {
-            this.app.showLoader("[data-loaderLecturers]");
-            const lecturers = await this.api.getLecturers(options);
-            return lecturers;
-        } catch (error) {
-            console.error(error);
-            return null;
-        }
-    }
-
     async init() {
-        this.lecturers = await this._fetchLecturers({ limit: 25 });
+        await this.loadActivities();
+        this.searchBar.on("input", () => this.fetchSearch());   
+        console.log("hiding loader");
+        await this.app.hideLoader();
+    }
 
-        if (!this.lecturers) {
-            this.noResults.show();
-            this.app.hideLoader('[data-loaderPage]');
+    async loadActivities(activities = null) {
+        this.activitiesList.empty();
+
+        if (!activities) {
+            // TODO: before, after, limit
+            console.log("[DEBUG] Activities are null, fetching...");
+            activities = await this.api.getActivities();
+        }
+
+        if (activities.length == 0) {
+            $("<h1>").text("Žádné aktivity nebyly nalezeny").addClass("col-span-3 mx-auto text-3xl font-bold").appendTo(this.activitiesList);
+            $("<p>").text("Začněte tím, že vytvoříte novou aktivitu.").addClass("col-span-3 mx-auto text-xl btn btn-big").appendTo(this.activitiesList);
+            return;            
+        }
+
+        activities.forEach(activity => this.renderActivity(activity));
+        console.log("done1");
+    }
+
+    async _fetchSearch() {
+        if (this.searchNow + 500 > Date.now()) {
+            console.log("[DEBUG] Search throttled")
             return;
         }
 
-        this.loadLecturers(this.lecturers);
-
-        
-        this.searchInput.on('input', this._filterBySearch.bind(this));
-
-        $(window).scroll(async () => {
-            const windowBottom = $(window).scrollTop() + $(window).height();
-            const gridBottom = this.lecturersList.offset().top + this.lecturersList.outerHeight();
-            
-            if (windowBottom <= gridBottom - 200) {
-                return;
-            }
-
-            if (!this.lecturers || this.lecturers.length <= 0) {
-                console.log('No items to load...');
-                return;
-            }
-            
-            if (this._fetchInProgress) {
-                console.log('Fetch in progress, skipping...');
-                return;
-            }
-
-            await this._fetchMoreLecturers();
-        });
-
-        this.app.hideLoader('[data-loaderPage]');
+        this.app.showLoader("[data-loader]");
+        const search = this.searchBar.val();
+        const activities = await this.api.searchActivities(search);
+        this.loadActivities(activities);
     }
 
-    loadLecturers(lecturers) {
-        if (lecturers.length <= 0) {
-            this.noResults.show();
-            this.app.hideLoader('[data-loaderLecturers]');
-            return;
+    async fetchSearch() {
+        this.searchNow = Date.now();
+        setTimeout(() => this._fetchSearch(), 500);
+    }
+
+    renderActivity(data) {
+        const activityBox = $("<div>").addClass("activity").appendTo(this.activitiesList);
+        activityBox.on("click", () => window.location.href = `/activity/${data.uuid}`);
+        if(data.gallery && Array.isArray(data.gallery)) {
+            $("<img>").attr("src", data.gallery[0].images[0].lowRes).addClass("activity-image").appendTo(activityBox);
         }
         
-        lecturers.forEach(lecturer => this.renderLecturer(lecturer));
-        this.lecturersList.show();
-        this.app.hideLoader('[data-loaderLecturers]');
-    }
-
-    filterLecturers() {
-
-    }
-
-    renderLecturer(data) {
-        
+        const activityDescription = $("<div>").addClass("flex flex-col").appendTo(activityBox);
+        $("<h2>").text(data.activityName).addClass("font-bold text-2xl").appendTo(activityDescription);
+        $("<p>").text(data.description).appendTo(activityDescription);
     }
 }
