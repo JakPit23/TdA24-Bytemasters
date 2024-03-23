8bb62cd9c0effc339e63b1a31cc634779d5b9a26
Jakub Pitner <jakub.pitner128@seznam.cz>
2024-03-23 12:23:36 +0100

    TdA -> AMOS

--- src/public/js/pages/ActivityPage.js
+++ src/public/js/pages/ActivityPage.js
@@ -31,9 +31,22 @@ class Page {
         const activityBox = $("<div>").addClass("activity activity-box").appendTo(this.activitiesList);
         const activityTitle = $("<h2>").text(data.activityName).addClass("font-bold text-3xl").appendTo(activityBox);
         const activityContent = $("<div>").addClass("flex flex-row").appendTo(activityBox);
-        $("<img>").attr("src", data.gallery[0].images[0].lowRes).appendTo(activityContent);
+        $("<img>").attr("src", data.gallery[0].images[0].lowRes).addClass("mr-4").appendTo(activityContent);
         const activityDescription = $("<div>").addClass("flex flex-col").appendTo(activityContent);
         $("<p>").text(data.description).appendTo(activityDescription);
-        $("<h3*>")
+        $("<h3>").text("Co si vyzkoušíš?").addClass("font-semibold text-2xl").appendTo(activityDescription);
+        data.objectives.forEach(objective => {
+            $("<p>").text(objective).appendTo(activityDescription);
+        })
+        data.edLevel.forEach(level => {
+            switch(level) {
+                case "secondarySchool":
+                    $("<div>").text("2. stupeň ZŠ").addClass("btn w-fit").appendTo(activityDescription);
+                    break;
+                case "highSchool":
+                    $("<div>").text("SŠ").addClass("btn w-fit").appendTo(activityDescription);
+                    break;
+            }
+        })
     }
 }
